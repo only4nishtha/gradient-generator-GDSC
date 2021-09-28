60e4762c824b62038a46fbda20a3ba0029faf624
Anshika Gautam <anshikagautam130@gmail.com>
2021-09-28 19:03:37 +0530

    multiple options

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var css = document.querySelector("h4");
+var code = document.querySelector("h4");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var gradient = document.getElementById("gradient");
+var typeGrad = document.querySelector("#type-grad");
 
+// linear-gradient(type_of_grad, color1,color2);
+// linear-gradient(45deg, color1.value, color2.value);
 function setGradient() {
- gradient.style.background = 
- "linear-gradient(45deg, " 
+ gradient.style.background = "linear-gradient("
+ + typeGrad.value
+ + ", "
  + color1.value 
  + ", " 
  + color2.value 
  + ")";
- css.textContent = gradient.style.background + ";";
+ code.innerText = gradient.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+typeGrad.addEventListener("input", setGradient);
